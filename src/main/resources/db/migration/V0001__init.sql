4ab54f722771c00bbcc9b8aaa09f49eb031ddbdd
chapay <BritikovKI@Yandex.ru>
2018-06-07 13:51:05 +0300

    Mambo No.11

--- src/main/resources/db/migration/V0001__init.sql
+++ src/main/resources/db/migration/V0001__init.sql
@@ -64,7 +64,7 @@ CREATE TABLE IF NOT EXISTS votes(
 CREATE TABLE IF NOT EXISTS forum_users(
   id SERIAL NOT NULL PRIMARY KEY ,
   forum CITEXT REFERENCES forums(slug),
-  nickname CITEXT REFERENCES users(nickname),
+  nickname CITEXT ,
   fullname TEXT ,
   email CITEXT NOT NULL ,
   about TEXT,
